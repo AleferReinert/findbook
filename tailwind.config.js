299735c8d019af2e2db1c039a01dc68a5917d483
Alefer Reinert <aleferreinert@gmail.com>
2024-06-09 02:14:55 -0300

    Fix url images

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['index.html', './src/**/**/*.{js,ts,jsx,tsx}'],
+	content: ['index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
 		extend: {
 			backgroundImage: {
-				header: `url('./src/assets/img/background-header.jpg')`,
-				float: `url('./src/assets/img/float-header.png')`
+				header: `url('src/assets/img/background-header.jpg')`,
+				float: `url('src/assets/img/float-header.png')`
 			},
 			boxShadow: {
 				default: '0 0 15px rgba(0,0,0,0.19)',
